e85107fa550d779b15a62e8bf088dc23f8aa57fb
steviecoaster <svalding@gmail.com>
2016-04-28 11:15:48 -0400

    Powershell module to speak insults to people
    Drop this in one of your module paths (Use $env:PSModulePath to get a list)
    
    Then within powershell run: Import-Module Hail-Insults
    
    you can then just run Hail-Insults as a cmdlet which will speak a random insult to you. 
    
    More insults can be added by entering them in the $insults array variable within the psm1 file in the Modules directory

---
+++ Hail-Insults/Hail-Insults.psm1
@@ -0,0 +1,16 @@
+﻿Function Hail-Insult {
+
+Add-Type -AssemblyName System.Speech
+
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+$speak.Rate = 0
+$speak.SelectVoiceByHints("female")
+
+$insults = ("You smell like burnt gym socks", "Why don't you do yourself a favor and die", "If you were any dumber I'd start calling you my ex-wife")
+
+$speak.Speak((Get-Random($insults)))
+
+}
+
+Export-ModuleMember -Function Hail-Insult
