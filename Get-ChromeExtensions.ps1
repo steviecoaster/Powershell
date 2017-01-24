109960005efe00e84542397ee320bdc045effa4d
Stephen Valdinger <svalding@kent.edu>
2017-01-24 13:46:40 -0500

    New File: Get-ChromeExtensions.ps1. Gets list of chrome extensions installed on a workstation.

---
+++ Get-ChromeExtensions.ps1
@@ -0,0 +1,28 @@
+﻿$targetdir = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Extensions"
+
+$extensions = Get-ChildItem $targetdir
+
+Foreach($ext in $extensions){
+
+Set-Location $targetdir\$ext -ErrorAction SilentlyContinue
+
+$folders = (Get-ChildItem).Name
+
+Foreach($folder in $folders){
+
+Set-Location $folder -ErrorAction SilentlyContinue
+
+
+
+$json = Get-Content manifest.json | ConvertFrom-Json
+
+$obj = New-Object System.Object
+
+$obj | Add-Member -MemberType NoteProperty -Name Name -Value $json.name
+$obj | Add-Member -MemberType NoteProperty -Name Version -Value $json.version
+
+Write-Output $obj
+}
+
+
+}
