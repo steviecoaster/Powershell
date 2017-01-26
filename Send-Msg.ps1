dbd81d855661957d69d330e015b316c62bd0c52a
Stephen Valdinger <svalding@kent.edu>
2017-01-26 09:44:32 -0500

    Wrapper script for using msg to send messages to users.

---
+++ Send-Msg.ps1
@@ -0,0 +1,17 @@
+﻿Function Send-Msg{
+
+Param(
+
+[cmdletBinding()]
+[Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
+[string]$Username,
+[Parameter(Mandatory=$true,Position=1,ValueFromPipeline=$true)]
+[string]$Computer,
+[Parameter(Mandatory=$true,Position=2)]
+[string]$MessageText
+
+)
+
+Start-Process msg -ArgumentList "$Username", "/server:$Computer", "$MessageText" -NoNewWindow
+
+}
