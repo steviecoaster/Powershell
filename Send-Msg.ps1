6495bbe08c47f7082c9b71babe59a0f0174d9772
Stephen Valdinger <svalding@kent.edu>
2017-01-26 10:30:04 -0500

    Added help information to top of script

--- Send-Msg.ps1
+++ Send-Msg.ps1
@@ -1,4 +1,36 @@
-﻿Function Send-Msg{
+﻿<#
+.SYNOPSIS
+
+Wrapper function for the built in msg command on Windows.
+
+.DESCRIPTION
+
+This script takes arguments from the function and forumlates a proper msg command.
+
+.PARAMETER Username
+
+The logged on user session to display the message in.
+
+.PARAMETER Computer
+
+The remote computer on which to display the message.
+
+.PARAMETER MessageText
+
+The content of the Message to be displayed.
+
+.EXAMPLE
+
+Send-Msg -Username joebob -Computer Wrkst01 -MessageText "This is a message from your friendly Sysadmin!"
+
+#>
+
+
+
+
+
+
+Function Send-Msg{
 
 Param(
 
