832e417b7cab3e38e96d35c1d24c65f63564d527
steviecoaster <steviecoaster@users.noreply.github.com>
2017-01-27 10:57:02 -0500

    Update Send-Msg.ps1
    Added indentation to make script more readable.

--- Send-Msg.ps1
+++ Send-Msg.ps1
@@ -32,18 +32,18 @@ Send-Msg -Username joebob -Computer Wrkst01 -MessageText "This is a message from
 
 Function Send-Msg{
 
-Param(
+  Param(
 
-[cmdletBinding()]
-[Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
-[string]$Username,
-[Parameter(Mandatory=$true,Position=1,ValueFromPipeline=$true)]
-[string]$Computer,
-[Parameter(Mandatory=$true,Position=2)]
-[string]$MessageText
+  [cmdletBinding()]
+  [Parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)]
+  [string]$Username,
+  [Parameter(Mandatory=$true,Position=1,ValueFromPipeline=$true)]
+  [string]$Computer,
+  [Parameter(Mandatory=$true,Position=2)]
+  [string]$MessageText
 
-)
+  )
 
-Start-Process msg -ArgumentList "$Username", "/server:$Computer", "$MessageText" -NoNewWindow
+  Start-Process msg -ArgumentList "$Username", "/server:$Computer", "$MessageText" -NoNewWindow
 
 }
