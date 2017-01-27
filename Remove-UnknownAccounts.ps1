22adece32ecb45968996125abbb7b0a1b15d343a
steviecoaster <steviecoaster@users.noreply.github.com>
2017-01-27 10:56:12 -0500

    Update Remove-UnknownAccounts.ps1
    Added Indentation to make script more readable.

--- Remove-UnknownAccounts.ps1
+++ Remove-UnknownAccounts.ps1
@@ -2,44 +2,44 @@
 
 Function Remove-UnknownAccounts{
 
-Param(
-[cmdletBinding()]
-[Parameter(Mandatory=$true,Position=0)]
-[switch]$Unknown
+  Param(
+  [cmdletBinding()]
+  [Parameter(Mandatory=$true,Position=0)]
+  [switch]$Unknown
 
-)
+  )
 
-Write-Verbose -Message "Starting...."
+  Write-Verbose -Message "Starting...."
 
-If($Unknown -eq $true){
+  If($Unknown -eq $true){
 
-Write-Verbose -Message "Moving into HKEY_USERS Hive..."
-Set-Location Registry::HKEY_USERS
+    Write-Verbose -Message "Moving into HKEY_USERS Hive..."
+    Set-Location Registry::HKEY_USERS
 
-Write-Verbose -Message "Collecting SIDs"
-$SID = ((Get-ChildItem -ErrorAction SilentlyContinue).Name).Substring(11)
+    Write-Verbose -Message "Collecting SIDs"
+    $SID = ((Get-ChildItem -ErrorAction SilentlyContinue).Name).Substring(11)
 
-}
+  }
 
-Foreach($s in $SID){
+  Foreach($s in $SID){
 
-Write-Verbose -Message "Processing SID: $s on $env:COMPUTERNAME..."
-If((Get-CimInstance win32_UserProfile | Select SID,Localpath | Where { $_.SID -match "$s"}) -eq ''){
+    Write-Verbose -Message "Processing SID: $s on $env:COMPUTERNAME..."
+    If((Get-CimInstance win32_UserProfile | Select SID,Localpath | Where { $_.SID -match "$s"}) -eq ''){
 
-Get-CimInstance win32_UserProfile | Where { $_.SID -eq $s} | Foreach {$_.Delete()}
+    Get-CimInstance win32_UserProfile | Where { $_.SID -eq $s} | Foreach {$_.Delete()}
 
-Write-Verbose -Message "$s removed from $env:COMPUTERNAME"
+    Write-Verbose -Message "$s removed from $env:COMPUTERNAME"
 
-}
+  }
 
-Else{
+  Else{
 
-Write-Verbose -Message "$s found in WMI User Table"
+    Write-Verbose -Message "$s found in WMI User Table"
 
-}
+  }
 
-}
+  }
 
-Write-Verbose -Message "All SIDS processed. System Cleaned SUCCESSFULLY"
+  Write-Verbose -Message "All SIDS processed. System Cleaned SUCCESSFULLY"
 
 }
